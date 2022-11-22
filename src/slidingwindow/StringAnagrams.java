9cdbb2d6f7ed135c88f7834b3d683845bc27ca89
Rohianon <rohianon48@gmail.com>
2022-11-22 21:30:20 +0300

    add solution for string anagrams #438

---
+++ src/slidingwindow/StringAnagrams.java
@@ -0,0 +1,48 @@
+package slidingwindow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StringAnagrams {
+    public static List<Integer> findStringAnagrams(String str, String pattern) {
+        int windowStart = 0, matched = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        for (char chr : pattern.toCharArray())
+            charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+
+        List<Integer> resultIndices = new ArrayList<>();
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+
+            char rightChar = str.charAt(windowEnd);
+            // decrement the frequency of the matched character
+            if (charFrequencyMap.containsKey(rightChar)) {
+                charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
+                if (charFrequencyMap.get(rightChar) == 0)
+                    matched++;
+            }
+
+            if (matched == charFrequencyMap.size())
+                resultIndices.add(windowStart);
+
+            if (windowEnd >= pattern.length() - 1) {//shrink the window
+                char leftChar = str.charAt(windowStart++);
+                if (charFrequencyMap.containsKey(leftChar)) {
+                    if (charFrequencyMap.get(leftChar) == 0)
+                        matched--; //before putting the character back, decrement the matched count
+                    // put the character back
+                    charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
+                }
+            }
+
+        }
+
+        return resultIndices;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringAnagrams.findStringAnagrams("ppqp", "pq"));
+        System.out.println(StringAnagrams.findStringAnagrams("abbcabc", "abc"));
+    }
+}
