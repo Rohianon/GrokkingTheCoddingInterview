255df407cf8ae938d89f22dae58d0a603fb8c5a3
Rohianon <rohianon48@gmail.com>
2022-12-20 10:55:51 +0300

    push solutions to RemoveElement

--- python_codes/twopointers/remove_element.py
+++ python_codes/twopointers/remove_element.py
@@ -5,3 +5,13 @@ def remove_element(arr, key):
             arr[next_element] = arr[i]
             next_element += 1
     return arr[:next_element]
+
+
+def main():
+  print("Array new length: " +
+        str(remove_element([3, 2, 3, 6, 3, 10, 9, 3], 3)))
+  print("Array new length: " +
+        str(remove_element([2, 11, 2, 2, 1], 2)))
+
+
+main()
